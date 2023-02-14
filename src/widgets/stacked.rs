9544b9969543c8cf746dc418f47eedd01b709b41
Adam A <adam@hack.bg>
2023-02-14 02:21:54 +0200

    stacked: re-enable rendering

--- src/widgets/stacked.rs
+++ src/widgets/stacked.rs
@@ -45,32 +45,32 @@ impl<'a> Output<Crossterm<'a>, (u16, u16)> for Stacked<'a, Crossterm<'a>, (u16, 
     fn render (&self, context: &mut Crossterm<'a>) -> Result<Option<(u16, u16)>> {
         let mut x = 0;
         let mut y = 0;
-        //match self.0 {
-            //Axis::X => {
-                //context.area.expect_min((self.1.len() as u16, 1))?; // FIXME height
-                //for item in self.1.iter() {
-                    //let (w, h) = Offset(x, 0, item).render(context)?.unwrap_or((0, 0));
-                    //x = x + w;
-                    //y = y.max(h);
-                //}
-            //},
-            //Axis::Y => {
-                //context.area.expect_min((1, self.1.len() as u16))?; // FIXME width
-                //for item in self.1.iter() {
-                    //let (w, h) = Offset(0, y, item).render(context)?.unwrap_or((0, 0));
-                    //x = x.max(w);
-                    //y = y + h;
-                //}
-            //},
-            //Axis::Z => {
-                //context.area.expect_min((1, 1 as u16))?; // FIXME size
-                //for item in self.1.iter().rev() {
-                    //let (w, h) = item.render(context)?.unwrap_or((0, 0));
-                    //x = x.max(w);
-                    //y = y.max(h);
-                //}
-            //}
-        //};
+        match self.0 {
+            Axis::X => {
+                context.area.expect_min((self.1.len() as u16, 1))?; // FIXME height
+                for item in self.1.iter() {
+                    let (w, h) = Offset(x, 0, item).render(context)?.unwrap_or((0, 0));
+                    x = x + w;
+                    y = y.max(h);
+                }
+            },
+            Axis::Y => {
+                context.area.expect_min((1, self.1.len() as u16))?; // FIXME width
+                for item in self.1.iter() {
+                    let (w, h) = Offset(0, y, item).render(context)?.unwrap_or((0, 0));
+                    x = x.max(w);
+                    y = y + h;
+                }
+            },
+            Axis::Z => {
+                context.area.expect_min((1, 1 as u16))?; // FIXME size
+                for item in self.1.iter().rev() {
+                    let (w, h) = item.render(context)?.unwrap_or((0, 0));
+                    x = x.max(w);
+                    y = y.max(h);
+                }
+            }
+        };
         Ok(Some((x, y)))
     }
 }
