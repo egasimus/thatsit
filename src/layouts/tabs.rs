b1d64c510f5130200b9df66e797ef6708a9c6e9d
Adam A <adam@hack.bg>
2023-02-20 20:37:48 +0200

    refactor(tabs)

--- src/layouts/tabs.rs
+++ src/layouts/tabs.rs
@@ -21,7 +21,60 @@ pub struct Tabbed<T> {
 
 impl<T> Tabbed<T> {
 
-    fn add_tabs <'a, U, V, W: Output<U, V> + Collection<'a, U, V>> (
+    /// Create a container holding the tab container and the active tab
+    pub fn layout <'a, U, V> (&'a self) -> Collected<'a, U, V> where
+        T:                 Output<U, V>,
+        String:            Output<U, V>,
+        Columns<'a, U, V>: Output<U, V>,
+        Rows<'a, U, V>:    Output<U, V>,
+        u16:               Output<U, V>,
+    {
+        let page = self.focus.and_then(|focus|self.pages.get(focus)).map(|page|&page.1);
+        match self.side {
+            None => match page {
+                Some(page) => return Collected::Ref(page),
+                None       => return Collected::None
+            }
+            Some(side) => {
+                let tabs = self.layout_tabs();
+                let space = if page.is_some() { 1u16 } else { 0u16 };
+                match side {
+                    TabSide::Left => {
+                        return Columns::new().add(tabs).add(space).add(page).into_collected()
+                    }
+                    TabSide::Top => {
+                        return Rows::new().add(tabs).add(space).add(page).into_collected()
+                    }
+                    TabSide::Right => {
+                        return Columns::new().add(page).add(space).add(tabs).into_collected()
+                    }
+                    TabSide::Bottom => {
+                        return Rows::new().add(page).add(space).add(tabs).into_collected()
+                    }
+                }
+            }
+        }
+    }
+
+    /// Create a container holding the tabs
+    pub fn layout_tabs <'a, U: 'a, V: 'a> (&'a self) -> Option<Box<dyn Output<U, V> + 'a>> where
+        String:            Output<U, V>,
+        Columns<'a, U, V>: Output<U, V> + Collection<'a, U, V>,
+        Rows<'a, U, V>:    Output<U, V> + Collection<'a, U, V>
+    {
+        match self.side {
+            None => None,
+            Some(side) => Some(match side {
+                TabSide::Left | TabSide::Right  =>
+                    Box::new(self.layout_tabs_in(Columns::new())),
+                TabSide::Top  | TabSide::Bottom =>
+                    Box::new(self.layout_tabs_in(Rows::new()))
+            })
+        }
+    }
+
+    /// Add the tabs to the passed container
+    pub fn layout_tabs_in <'a, U, V, W: Output<U, V> + Collection<'a, U, V>> (
         &'a self, mut container: W
     ) -> W where String: Output<U, V> {
         let selected = self.focus;
@@ -39,51 +92,6 @@ impl<T> Tabbed<T> {
             }
         }
         container
-    }
-
-    pub fn tabs <'a, U: 'a, V: 'a> (&'a self) -> Option<Box<dyn Output<U, V> + 'a>> where
-        String:            Output<U, V>,
-        Columns<'a, U, V>: Output<U, V> + Collection<'a, U, V>,
-        Rows<'a, U, V>:    Output<U, V> + Collection<'a, U, V>
-    {
-        match self.side {
-            None => None,
-            Some(side) => Some(match side {
-                TabSide::Left | TabSide::Right  =>
-                    Box::new(self.add_tabs(Columns::new())),
-                TabSide::Top  | TabSide::Bottom =>
-                    Box::new(self.add_tabs(Rows::new()))
-            })
-        }
-    }
-
-    pub fn layout <'a, U, V> (&'a self) -> Collected<'a, U, V> where
-        T:                 Output<U, V>,
-        String:            Output<U, V>,
-        Columns<'a, U, V>: Output<U, V>,
-        Rows<'a, U, V>:    Output<U, V>,
-        u16:               Output<U, V>,
-    {
-        let page  = self.focus.and_then(|focus|self.pages.get(focus)).map(|page|&page.1);
-        let space = if page.is_some() { 1u16 } else { 0u16 };
-        match self.side {
-            Some(TabSide::Left) => {
-                return Columns::new().add(self.tabs()).add(space).add(page).into_collected()
-            },
-            Some(TabSide::Top) => {
-                return Rows::new().add(self.tabs()).add(space).add(page).into_collected()
-            },
-            Some(TabSide::Right) => {
-                return Columns::new().add(page).add(space).add(self.tabs()).into_collected()
-            },
-            Some(TabSide::Bottom) => {
-                return Rows::new().add(page).add(space).add(self.tabs()).into_collected()
-            },
-            None => match page {
-                Some(page) => return Collected::Ref(page),
-                None       => return Collected::None
-            }
-        }
     }
 
 }
